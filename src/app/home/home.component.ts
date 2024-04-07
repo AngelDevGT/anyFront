fc9e122deec6436a4c5d0a303e1641b69b5ca7fa
Ivan Racancoj <ivan.racancoj@blautech.us>
2024-04-07 11:00:56 -0600

    Home dashboard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { User } from '@app/models/system/user.model';
-import { AccountService } from '@app/services';
+import { AccountService, DataService } from '@app/services';
 import {
 AbstractControl,
 FormBuilder,
@@ -9,18 +9,119 @@ FormGroup,
 Validators,
 FormControl,
 } from '@angular/forms';
+import { RawMaterialOrder } from '@app/models/raw-material/raw-material-order.model';
+import { forkJoin } from 'rxjs';
+import { ProductForSaleStoreOrder } from '@app/models/product-for-sale/product-for-sale-store-order.model';
+import { ShopResume } from '@app/models/store/shop-resume.model';
+import { CashClosing } from '@app/models/store/cash-closing.model';
 
 @Component({ 
     selector: 'app-home',
     templateUrl: 'home.component.html',
     styleUrls: ['home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-    user: User | null;
+    isLoading = true;
+    rawMaterialOrders?: RawMaterialOrder[];
+    productForSaleOrdes?: ProductForSaleStoreOrder[];
+    shopResumes?: ShopResume[];
+    cashClosings?: CashClosing[];
+    stats?: any = {};
 
-    constructor(private accountService: AccountService, public _builder: FormBuilder) {
-        this.user = this.accountService.userValue;
+    constructor(private dataService: DataService) {
+        
+    }
+
+    ngOnInit() {
+        
+        let requestArray = [];
+
+        requestArray.push(this.dataService.getAllRawMaterialOrderByFilter({"status": 1}));
+        requestArray.push(this.dataService.getAllProducForSaleOrder());
+        requestArray.push(this.dataService.getShopHistory({}));
+        requestArray.push(this.dataService.getAllCashClosingByFilter({}));
+        forkJoin(requestArray).subscribe({
+            next: (result: any) => {
+                this.rawMaterialOrders = result[0].retrieveRawMaterialOrderResponse?.rawMaterial;
+                this.productForSaleOrdes = result[1].retrieveProductForSaleStoreOrderResponse?.saleStoreOrder;
+                this.shopResumes = result[2].retrieveShopHistoryResponse?.FinishedProducts;
+                // this.cashClosings = result[3].retrieveStoreCashClosingResponse?.StoreCashClosing;
+            },
+            error: (e) =>  console.error('Se ha producido un error al realizar una(s) de las peticiones', e),
+            complete: () => {
+                this.setStats();
+                this.isLoading = false;
+            }
+        });
+    }
+
+    setStats(){
+        let rawMaterialOrdersStats = {
+            total: 0,
+            pending: 0,
+            received: 0,
+            verified: 0
+        };
+
+        let productForSaleOrdersStats = {
+            total: 0,
+            pending: 0,
+            received: 0,
+            ready: 0
+        };
+
+        let storeSalesStats = {
+            total: this.shopResumes?.length,
+            pending: 0,
+            received: 0,
+            ready: 0
+        };
+
+        // let cashClosingStats = {
+        //     total: 0,
+        //     pending: 0,
+        //     active: 0,
+        //     verified: 0
+        // };
+
+        this.rawMaterialOrders?.forEach((order) => {
+            rawMaterialOrdersStats.total++;
+            if(order.status?.id == 4){
+                rawMaterialOrdersStats.pending++;
+            } else if(order.status?.id == 10){
+                rawMaterialOrdersStats.verified++;
+            } else if(order.status?.id == 7){
+                rawMaterialOrdersStats.received++;
+            }
+        });
+        
+        this.productForSaleOrdes?.forEach((order) => {
+            productForSaleOrdersStats.total++;
+            if(order.storeStatus?.id == 1){
+                productForSaleOrdersStats.pending++;
+            } else if(order.storeStatus?.id == 7){
+                productForSaleOrdersStats.ready++;
+            } else if(order.storeStatus?.id == 3){
+                productForSaleOrdersStats.received++;
+            }
+        });
+
+        // this.cashClosings?.forEach((order) => {
+        //     cashClosingStats.total++;
+        //     if(order.status?.id == 10){
+        //         cashClosingStats.verified++;
+        //     } else if(order.status?.id == 2){
+        //         cashClosingStats.active++;
+        //     }
+        
+        // });
+
+        this.stats.rawMaterialOrders = rawMaterialOrdersStats;
+        this.stats.productForSaleOrders = productForSaleOrdersStats;
+        this.stats.shopResumes = storeSalesStats;
+        // this.stats.cashClosing = cashClosingStats;
+
     }
 
 }
