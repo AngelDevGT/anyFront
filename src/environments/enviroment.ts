3758a2b65e7115568e1255d059782e5015f9d043
Ivan Racancoj <ivan.racancoj@blautech.us>
2025-09-22 08:16:20 -0600

    Ajuste angular environments

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     // apiUrl: 'http://localhost:7071/api'
     // apiUrl: 'https://func-embutidos-any-qa.azurewebsites.net/api',
-    apiUrlV3: '/api',
+    apiUrlV3: 'https://any-function-sql.azurewebsites.net/api/V3',
     apiUrlV2: 'https://func-embutidos-any-qa.azurewebsites.net/api',
     // apiUrl: 'http://any-function-sql.azurewebsites.net/api/V3',
 };
