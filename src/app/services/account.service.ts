15653470a9f9728341c41f6e211bad10d8c91d56
Ivan Racancoj <ivan.racancoj@blautech.com>
2026-08-11 08:33:47 -0600

    update: disabled ventas consultas

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -398,14 +398,14 @@ const menuItemsOptions: any = [
         icon_name: 'arrow_right',
         icon_class: 'material-icons icon',
       },
-      {
-        root_class: 'list-group list-group-flush',
-        router_link: '/consultas/ventas',
-        link_class: 'list-group-item py-2 ripple',
-        link_name: 'Ventas',
-        icon_name: 'arrow_right',
-        icon_class: 'material-icons icon',
-      },
+      // {
+      //   root_class: 'list-group list-group-flush',
+      //   router_link: '/consultas/ventas',
+      //   link_class: 'list-group-item py-2 ripple',
+      //   link_name: 'Ventas',
+      //   icon_name: 'arrow_right',
+      //   icon_class: 'material-icons icon',
+      // },
     ],
   },
 ];
